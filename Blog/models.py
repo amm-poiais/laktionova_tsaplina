8fb3ae2eea07e8059c04dbf1019135b603f7e8f5
DankeMary <laktionofs@mail.ru>
2017-12-21 14:45:05 +0300

    Models created

--- Blog/models.py
+++ Blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class News(models.Model):
+    title = models.CharField(max_length=128)
+    text = models.CharField(max_length=255)
+    timestamp = models.DateTimeField()
+    user = models.ForeignKey('UserProfile', related_name='author')
+    category = models.ForeignKey('Category')
+    attachment = models.BinaryField()
+    accepted = models.BooleanField()
+    comment = models.CharField(max_length=255)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    def __str__(self):
+        return 'Username: %s; email: %s' % (self.user.username, self.user.email)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
