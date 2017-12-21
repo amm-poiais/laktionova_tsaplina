5cc877ff856380071e6ea3e6088699b280a05c77
DankeMary <laktionofs@mail.ru>
2017-12-21 17:33:07 +0300

    (don't look at this one)

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django import forms
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.contrib.auth import login as django_login
 # Create your views here.
+
+
+
