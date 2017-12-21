a331d1300abaa9a7fe3fe33186b75489ff8e97b7
DankeMary <laktionofs@mail.ru>
2017-12-21 16:52:45 +0300

    Model News updated

--- Blog/models.py
+++ Blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 
 # Create your models here.
@@ -7,15 +8,19 @@ class News(models.Model):
     title = models.CharField(max_length=128)
     text = models.CharField(max_length=255)
     timestamp = models.DateTimeField()
-    user = models.ForeignKey('UserProfile', related_name='author')
-    category = models.ForeignKey('Category')
+    user = models.ForeignKey('UserProfile', related_name='author', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     attachment = models.BinaryField()
     accepted = models.BooleanField()
     comment = models.CharField(max_length=255)
 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return 'Username: %s; email: %s' % (self.user.username, self.user.email)
