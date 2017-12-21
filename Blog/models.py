13b71a1956953898cccf5c322b1122ba8e60b783
Lilia_Tsaplina <foxtrot7777@mail.ru>
2017-12-21 17:10:11 +0300

    Arguments on_delete added

--- Blog/models.py
+++ Blog/models.py
@@ -7,15 +7,15 @@ class News(models.Model):
     title = models.CharField(max_length=128)
     text = models.CharField(max_length=255)
     timestamp = models.DateTimeField()
-    user = models.ForeignKey('UserProfile', related_name='author')
-    category = models.ForeignKey('Category')
+    user = models.ForeignKey('UserProfile', related_name='author', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
     attachment = models.BinaryField()
     accepted = models.BooleanField()
     comment = models.CharField(max_length=255)
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return 'Username: %s; email: %s' % (self.user.username, self.user.email)
