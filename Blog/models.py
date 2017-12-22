7ad803a3373424a23c1740c37b97983951b77732
DankeMary <laktionofs@mail.ru>
2017-12-22 10:39:23 +0300

    Erased UserProfile model

--- Blog/models.py
+++ Blog/models.py
@@ -8,7 +8,7 @@ class News(models.Model):
     title = models.CharField(max_length=128)
     text = models.CharField(max_length=255)
     timestamp = models.DateTimeField()
-    user = models.ForeignKey('UserProfile', related_name='author', on_delete=models.CASCADE)
+    user = models.ForeignKey('User', related_name='author', on_delete=models.CASCADE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     attachment = models.BinaryField()
     status = models.ForeignKey('NewsStatus', on_delete=models.CASCADE)
@@ -17,13 +17,6 @@ class News(models.Model):
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    def __str__(self)                 :
-        return 'Username: %s; email: %s' % (self.user.username, self.user.email)
 
 
 class Category(models.Model):
