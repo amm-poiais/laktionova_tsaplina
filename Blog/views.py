d406d91a3a9c7f1cf84a66313408df1c723be920
DankeMary <laktionofs@mail.ru>
2017-12-23 21:11:00 +0300

    Raw login method & login page

--- Blog/views.py
+++ Blog/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
-
-from django import forms
-from django.http import HttpResponse
 from django.contrib.auth import authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login as django_login
-# Create your views here.
+from django.http import HttpResponseRedirect
 
 
+def login(request):
+    if request.method == 'POST':
+        user = authenticate(
+            request,
+            username=request.POST['username'],
+            password=request.POST['password'])
+        if user is not None:
+            django_login(request, user)
+            return HttpResponseRedirect('/users')
+        else:
+            return HttpResponseRedirect('/login')
+    else:
+        form = AuthenticationForm()
+        return render(
+            request,
+            'login.html',
+            {'login_form': form})
 
