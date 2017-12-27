dffad0ef0167d022abc687523d3a7ccfd18346b3
DankeMary <laktionofs@mail.ru>
2017-12-27 09:51:18 +0300

    Changed dir for saving photos from posts

--- Blog/models.py
+++ Blog/models.py
@@ -3,14 +3,17 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 
-# Create your models here.
+def user_directory_path(instance, filename):
+    return '{0}/{1}'.format(instance.user.username, filename)
+
+
 class News(models.Model):
     title = models.CharField(max_length=128)
     text = models.CharField(max_length=255)
     timestamp = models.DateTimeField()
     user = models.ForeignKey(User, related_name='author', on_delete=models.CASCADE)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
-    attachment = models.ImageField(upload_to='user', blank=True, null=True)
+    attachment = models.ImageField(upload_to=user_directory_path, blank=True, null=True)
     status = models.ForeignKey('NewsStatus', on_delete=models.CASCADE)
     comment = models.CharField(max_length=255, blank=True, null=True)
 
