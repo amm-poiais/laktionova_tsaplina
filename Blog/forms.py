c12e4e11bc9fa50798f6266210bab49488f4c816
DankeMary <laktionofs@mail.ru>
2017-12-28 06:07:52 +0300

    Fixed NewsForm (required fields added)

--- Blog/forms.py
+++ Blog/forms.py
@@ -5,8 +5,8 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class NewsForm(ModelForm):
-    title = CharField(max_length=128, required=False)
-    text = CharField(max_length=255, required=False,
+    title = CharField(max_length=128, required=True)
+    text = CharField(max_length=255, required=True,
                      widget=Textarea(attrs={'cols': 30, 'rows': 10}))
     attachment = ImageField(label=_('Pic'),required=False,
                             widget=FileInput)
